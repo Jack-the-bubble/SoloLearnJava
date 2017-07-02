470b42c1423a74be3fca5954cfdaa04035fa5423
Jack-the-bubble <ms1b.139@gmail.com>
2017-07-02 23:18:39 +0200

    Pierwszy upload
    Classes refering to first module of SoloLearn in Java

---
+++ JavaSoloLearn1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class JavaSoloLearn1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "David";
+		int age=42;
+		double score=0.72;
+		char group='Z', data='C';
+		Boolean online=true, offline=false;
+		
+		
+		
+		//Primitive operators
+		
+		int sum1=3, sum2=4;
+		int sum3=sum1+sum2;
+		System.out.println(sum3);
+		
+		//increment and decrement
+		
+		int x=34;
+		int y=++x;
+		System.out.println(y);
+		y=x++;
+		System.out.println(y);
+		System.out.println(x);
+		
+		//assingment operators
+		
+		double ant=0.65;
+		double any=566;
+		any*=ant;
+		System.out.println(any);
+		
+		//Strings
+		
+		String one, two;
+		one="Mariah";
+		two="Carrey";
+		System.out.println("Her name was " + one+" "+two);
+	
+		
+
+	}
+
+}
---
+++ Skaner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Skaner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//scanner
+		
+		Scanner myVar=new Scanner(System.in);
+		System.out.println(myVar.nextLine());
+	}
+
+}
